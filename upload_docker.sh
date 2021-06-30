efea0f6915e6521f5f2653604c6208b1211b5774
mehinkle <me.hinkle0@gmail.com>
2021-06-29 21:48:03 -0400

    tasks thru 5

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="mhink/devopsmicroservices"
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag devopsmicroservices $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
