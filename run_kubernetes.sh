40cac70b69d34ee66ebe8aa0ba1c8af7dd511db4
mehinkle <me.hinkle0@gmail.com>
2021-06-29 22:20:03 -0400

    task 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="mhink/devopsmicroservices"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image=$dockerpath devops --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops 8000:80
